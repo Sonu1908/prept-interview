3cc8b4e9c522c5eb53fe7e43cf2bc4c9af99df3b
Sonu1908 <gsonu232@gmail.com>
2026-06-01 23:29:44 +0530

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,60 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import { DM_Sans, Lora } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Header from "@/components/Header";
+import { Toaster } from "sonner";
+
+
+const lora = Lora({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  style: ["normal", "italic"],
+  variable: "--font-serif",
+});
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Prept",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        theme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={`${lora.variable} ${dmSans.variable} font-sans`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+
+            {/* Header */}
+            <Header/>
+            
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors/>
+           
+            {/* Footer */}
+            <footer className="relative z-10 border-t border-white/7 py-12  mx-auto px-6 flex flex-wrap items-center justify-center text-stone-400">
+              Made ❤️ by Sonu@developer
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ proxy.js
@@ -0,0 +1,55 @@
+import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from "next/server";
+import arcjet, { detectBot, shield } from "@arcjet/next";
+
+const isProtectedRoute = createRouteMatcher([
+  "/appointments(.*)",
+  "/explore(.*)",
+  "/dashboard(.*)",
+  "/onboarding(.*)",
+]);
+
+// Boat Protections
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW"],
+    }),
+  ],
+});
+
+export default clerkMiddleware(async (auth, req) => {
+
+  // Apply Arcjet protection FIRST (before Clerk auth check)
+  const decision = await aj.protect(req);
+
+   if (decision.isDenied()) {
+      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+    }
+  
+
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
+});
+
+
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for Clerk's auto-proxy path
+    '/__clerk/(.*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
