e2b171969b68713eb14d82c8bf8b91d62def8d71
Sonu1908 <gsonu232@gmail.com>
2026-06-13 23:31:50 +0530

    Final changes

--- app/(main)/call/[callId]/page.jsx
+++ app/(main)/call/[callId]/page.jsx
@@ -1,7 +1,8 @@
+
 import { redirect, notFound } from "next/navigation";
 import { getCallData } from "@/actions/call";
 import CallRoom from "./_components/CallRoom";
-import { toast } from "sonner";
+
 
 export default async function CallPage({ params }) {
   const { callId } = await params;
@@ -9,15 +10,12 @@ export default async function CallPage({ params }) {
   const result = await getCallData(callId);
 
   if (result.error === "Unauthorized") {
-    toast.error("You must be signed in to access this call")
     redirect("/");
   }
   if (result.error === "Call not found") {
-    toast.error("This call does not exit")
     notFound();
   }
   if (result.error === "Forbidden") {
-    toast.error("you do not have permission to access this call")
     redirect("/");
   }
 
